814bcdfd36453cfb7ea889ff59c02792695611b7
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2023-07-10 21:50:34 +0200

    Updating shell script to handle whitespaces and to make it better readable

--- data/git-clone-repos.sh
+++ data/git-clone-repos.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
-# Read the repository and commit entries from the CSV file
+# Function to process each line in the CSV file
+process_csv_line() {
+  repository=$1
+  commit=$2
+
+  # Trim leading/trailing whitespace from the commit
+  commit=$(echo "$commit" | awk '{$1=$1};1')
+
+  # Extract the repository name from the URL
+  repo_name=$(basename "$repository" .git)
+
+  # Clone the repository
+  git clone --depth 1 "$repository"
+
+  # Enter the cloned repository directory
+  cd "$repo_name" || exit
+
+  # Checkout the desired commit
+  git checkout "$commit"
+
+  # Return to the original directory
+  cd .. || exit
+}
+
+# Read the CSV file and process each line
 while IFS=, read -r repository commit; do
-    # Clone the repository
-    git clone --depth 1 "$repository"
-
-    # Enter the cloned repository directory
-    repo_name=$(basename "$repository" .git)
-    cd "$repo_name" || exit
-
-    # Checkout the desired commit
-    git checkout "$commit"
-
-    # Return to the original directory
-    cd ..
+  process_csv_line "$repository" "$commit"
 
 # Modify the CSV file name as needed
-done < top1javawithCheckstyle-2023-06-07-main.csv
+done < repos.csv
