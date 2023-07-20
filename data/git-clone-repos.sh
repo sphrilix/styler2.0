cb0a135711430c698c490d90ec39f3c019715cdd
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2023-07-20 14:51:38 +0200

    Improving download shell skript

--- data/git-clone-repos.sh
+++ data/git-clone-repos.sh
@@ -11,22 +11,39 @@ process_csv_line() {
   # Extract the repository name from the URL
   repo_name=$(basename "$repository" .git)
 
-  # Clone the repository
-  git clone --depth 1 "$repository"
+  # Check if the repository directory already exists
+  if [ -d "$repo_name" ]; then
+    echo "Repository '$repo_name' already exists. Skipping clone and checkout."
+  else
+    # Clone the repository
+    git clone --depth 1 "$repository"
 
-  # Enter the cloned repository directory
-  cd "$repo_name" || exit
+    # Enter the cloned repository directory
+    cd "$repo_name" || exit
 
-  # Checkout the desired commit
-  git checkout "$commit"
+    # Checkout the desired commit
+    git checkout "$commit"
 
-  # Return to the original directory
-  cd .. || exit
+    # Return to the original directory
+    cd .. || exit
+  fi
 }
+
+# Check if the CSV file argument is provided
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <csv_file>"
+  exit 1
+fi
+
+csv_file="$1"
+
+# Check if the CSV file exists
+if [ ! -f "$csv_file" ]; then
+  echo "CSV file not found: $csv_file"
+  exit 1
+fi
 
 # Read the CSV file and process each line
 while IFS=, read -r repository commit; do
   process_csv_line "$repository" "$commit"
-
-# Modify the CSV file name as needed
-done < repos.csv
+done < "$csv_file"
