87c3589fba47baa432d00c04cab264e179cd88c7
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2023-07-20 18:17:40 +0200

    Adding shell script for running CHECKSTYLE on multiple projects

---
+++ checkstyle.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Usage function to show how to use the script
+usage() {
+    echo "Usage: $0 <input_directory> <output_directory>"
+    echo "Example: $0 data/ output/"
+    exit 1
+}
+
+# Check if the correct number of arguments is provided
+if [ $# -ne 2 ]; then
+    usage
+fi
+
+# Store the input and output directories provided as arguments
+input_dir="$1"
+output_dir="$2"
+
+# Check if the input directory exists
+if [ ! -d "$input_dir" ]; then
+    echo "Error: Input directory '$input_dir' does not exist."
+    exit 1
+fi
+
+# Create the output directory if it doesn't exist
+mkdir -p "$output_dir"
+
+# Save the current directory
+current_dir="$PWD"
+
+# Check if Poetry environment is already set up
+if ! poetry env info &> /dev/null; then
+    echo "Poetry environment not found. Installing dependencies..."
+    poetry install
+fi
+
+# Export PYTHONPATH
+export PYTHONPATH="${PYTHONPATH}:${current_dir}"
+
+# Iterate over each directory in the "data" directory
+for directory in "$input_dir"/*; do
+  if [ -d "$directory" ]; then
+    echo "Processing directory: $directory"
+
+    # Get the directory name without the path
+    dir_name=$(basename "$directory")
+
+    # Create a subfolder for each input folder in the "output" directory
+    output_subdir="$output_dir/${dir_name}"
+    mkdir -p "$output_subdir"
+
+    # Run the command for each directory separately
+    poetry run python src/styler2_0/main.py CHECKSTYLE --save "$output_subdir" --source "$directory" --tested
+  fi
+done
