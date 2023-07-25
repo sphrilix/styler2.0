598fb6f5b8d67e5c1658851b13eaceb9b04662b0
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2023-07-25 14:11:56 +0200

    Checking version parameter of checkstyle in pom.xml in case it is a variable with ${...}

--- src/styler2_0/utils/maven.py
+++ src/styler2_0/utils/maven.py
@@ -17,6 +17,7 @@ POM_XML = "pom.xml"
 STANDARD_NAMESPACE = "http://maven.apache.org/POM/4.0.0"
 CHECKSTYLE_PLUGIN_ARTIFACT_ID = "maven-checkstyle-plugin"
 CHECKSTYLE_ARTIFACT_ID = "checkstyle"
+DEPENDENCY_REGEX = re.compile(r"\$\{[^}]+}")
 
 
 class MavenException(Exception):
@@ -38,13 +39,11 @@ def _get_checkstyle_version_from_pom(pom: Path) -> str:
     parsed_pom = Xml.parse(pom)
     root = parsed_pom.getroot()
     namespaces = _get_namespaces(root)
-    for plugin in root.findall(
-        ".//xmlns:build/xmlns:plugins/xmlns:plugin", namespaces=namespaces
-    ):
+    for plugin in root.findall(".//xmlns:plugin", namespaces=namespaces):
         name = plugin.find("xmlns:artifactId", namespaces=namespaces).text
         if name != CHECKSTYLE_PLUGIN_ARTIFACT_ID:
             continue
-        return _parse_checkstyle_version_from_xml_elem(plugin, namespaces)
+        return _parse_checkstyle_version_from_xml_elem(plugin, root, namespaces)
     raise MavenException("Project does not support checkstyle.")
 
 
@@ -54,7 +53,7 @@ def _get_namespaces(root: Xml.Element) -> dict[str, str]:
 
 
 def _parse_checkstyle_version_from_xml_elem(
-    elem: Xml.Element, namespaces: dict[str, str]
+    elem: Xml.Element, root, namespaces: dict[str, str]
 ) -> str:
     checkstyle_dependencies = elem.findall(".//xmlns:dependency", namespaces=namespaces)
     for dependency in checkstyle_dependencies:
@@ -64,8 +63,14 @@ def _parse_checkstyle_version_from_xml_elem(
             if version:
                 return version
     plugin_version = elem.find("xmlns:version", namespaces=namespaces).text
-    if plugin_version and plugin_version in MAVEN_PLUGIN_CHECKSTYLE_VERSION:
-        return MAVEN_PLUGIN_CHECKSTYLE_VERSION[plugin_version]
+    if plugin_version:
+        if DEPENDENCY_REGEX.match(plugin_version):
+            plugin_version = _parse_checkstyle_version_from_variable(
+                plugin_version, root, namespaces
+            )
+        if plugin_version in MAVEN_PLUGIN_CHECKSTYLE_VERSION:
+            return MAVEN_PLUGIN_CHECKSTYLE_VERSION[plugin_version]
+
     raise MavenException("Not supported checkstyle version.")
 
 
@@ -77,3 +82,20 @@ def get_checkstyle_version_of_project(project_dir: Path) -> str:
     """
     pom = _find_pom_xml(project_dir)
     return _get_checkstyle_version_from_pom(pom)
+
+
+def _parse_checkstyle_version_from_variable(variable, root, namespaces):
+    """
+    Parse checkstyle version from variable.
+    :param variable: Variable to parse.
+    :param root: Root of pom.
+    :param namespaces: Namespaces of pom.
+    :return: Return the checkstyle version. If not found, return None.
+    """
+    variable = variable[2:-1]
+    for prop in root.findall(".//xmlns:properties", namespaces=namespaces):
+        for child in prop:
+            if child.tag == f"{{{STANDARD_NAMESPACE}}}{variable}":
+                return child.text
+
+    return None
