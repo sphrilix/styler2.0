fec8a6d3d372f013ab21b8e82aa0449d6e27d325
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2023-07-26 20:08:36 +0200

    Method extractor working

--- src/styler2_0/post-processing/method_extractor.py
+++ src/styler2_0/post-processing/method_extractor.py
@@ -1,11 +1,13 @@
 import logging
 import os
+from typing import Any
 
 import javalang
+from javalang.tree import MethodDeclaration
 
 from src.styler2_0.main import setup_logging
 
-INPUT_DIR = r"D:\PyCharm_Projects_D\styler2.0\Tmp"
+INPUT_DIR = r"D:\PyCharm_Projects_D\styler2.0\extracted"
 OUTPUT_DIR = r"D:\PyCharm_Projects_D\styler2.0\methods"
 
 
@@ -48,25 +50,34 @@ def _extract_methods_from_file(input_file: str, output_dir: str) -> None:
             w.write(method_code)
 
 
-def _iterate_methods(file: str) -> dict:
+def _iterate_methods(file: str) -> dict[str, str]:
     """
     Iterates over the methods in a file and returns a dictionary containing the method
     name and the method code.
     :param file: The file.
     :return: A dictionary containing the method name and the method code.
     """
+    # Read the file
     with open(file) as r:
         codelines = r.readlines()
         code_text = "".join(codelines)
 
-    lex = None
-    parse_tree = javalang.parse.parse(code_text)
     methods = {}
-    for _, method_node in parse_tree.filter(javalang.tree.MethodDeclaration):
-        startpos, endpos, startline, endline = get_method_start_end(
+
+    # Try to parse the file
+    lex = None
+    try:
+        parse_tree = javalang.parse.parse(code_text)
+    except javalang.parser.JavaSyntaxError as e:
+        logging.warning("Could not parse file %s.", file)
+        logging.warning(e)
+        return {}
+
+    for _, method_node in parse_tree.filter(MethodDeclaration):
+        startpos, endpos, startline, endline = _get_method_start_end(
             parse_tree, method_node
         )
-        method_text, startline, endline, lex = get_method_text(
+        method_text, startline, endline, lex = _get_method_text(
             codelines, startpos, endpos, startline, endline, lex
         )
         methods[method_node.name] = method_text
@@ -74,11 +85,21 @@ def _iterate_methods(file: str) -> dict:
     return methods
 
 
-def get_method_start_end(parse_tree, method_node):
+def _get_method_start_end(
+    parse_tree: list, method_node: MethodDeclaration
+) -> tuple[str, str, int, int]:
+    """
+    Get the start and end position of a method in the source code.
+    :param parse_tree: The full parse tree.
+    :param method_node: The method node.
+    :return: The start and end position of the method.
+    """
     startpos = None
     endpos = None
     startline = None
     endline = None
+
+    # Iterate over the parse tree and find the method node
     for path, node in parse_tree:
         if startpos is not None and method_node not in path:
             endpos = node.position
@@ -90,12 +111,28 @@ def get_method_start_end(parse_tree, method_node):
     return startpos, endpos, startline, endline
 
 
-def get_method_text(
-    codelines, startpos, endpos, startline, endline, last_endline_index
-):
+def _get_method_text(
+    codelines: list[str],
+    startpos: str,
+    endpos: str,
+    startline: int,
+    endline: int,
+    last_endline_index: int,
+) -> tuple[str, int | None, int | None, Any]:
+    """
+    Get the text of a method.
+    :param codelines: The code lines.
+    :param startpos: The start position of the method.
+    :param endpos: The end position of the method.
+    :param startline: The start line of the method.
+    :param endline: The end line of the method.
+    :param last_endline_index: The index of the last end line.
+    :return: The text of the method.
+    """
     if startpos is None:
         return "", None, None, None
 
+    # Get the start and end line index
     startline_index = startline - 1
     endline_index = endline - 1 if endpos is not None else None
 
@@ -117,6 +154,7 @@ def get_method_text(
             meth_text = meth_text[: meth_text.rfind("}")]
             meth_text = meth_text[: meth_text.rfind("}") + 1]
 
+    # 3. remove any trailing comments
     meth_lines = meth_text.split("<ST>")
     meth_text = "".join(meth_lines)
     last_endline_index = startline_index + (len(meth_lines) - 1)
@@ -139,7 +177,9 @@ def main():
 
     # Iterate over each directory in the input directory
     for directory in os.listdir(INPUT_DIR):
-        _extract_methods_from_dir(os.path.join(INPUT_DIR, directory), OUTPUT_DIR)
+        # Create a subfolder for each directory in the output directory
+        output_subdir = os.path.join(OUTPUT_DIR, directory)
+        _extract_methods_from_dir(os.path.join(INPUT_DIR, directory), output_subdir)
 
 
 if __name__ == "__main__":
