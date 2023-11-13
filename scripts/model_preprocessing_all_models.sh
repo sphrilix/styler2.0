7be033759f7700b26b1b232543bf128718922e7f
sphrilix <maxi.jungwirth@gmail.com>
2023-11-13 16:16:58 +0000

    add preprocessing of all models

--- scripts/model_preprocessing_all_models.sh
+++ scripts/model_preprocessing_all_models.sh
@@ -3,3 +3,9 @@ poetry run styler2_0 PREPROCESSING --project_dir "$1" --model TRANSFORMER
 
 echo "Preprocessing $1 for model LSTM"
 poetry run styler2_0 PREPROCESSING --project_dir "$1" --model LSTM
+
+echo "Preprocessing $1 for model ANN"
+poetry run styler2_0 PREPROCESSING --project_dir "$1" --model ANN
+
+echo "Preprocessing $1 for model N_GRAM"
+poetry run styler2_0 PREPROCESSING --project_dir "$1" --model N_GRAM
