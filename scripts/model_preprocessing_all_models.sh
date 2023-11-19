50db11188621b0fa6d1c7af65659d49ab8483f83
sphrilix <maxi.jungwirth@gmail.com>
2023-11-19 12:24:01 +0000

    fix ann

--- scripts/model_preprocessing_all_models.sh
+++ scripts/model_preprocessing_all_models.sh
@@ -7,5 +7,5 @@ poetry run styler2_0 PREPROCESSING --project_dir "$1" --model LSTM
 echo "Preprocessing $1 for model ANN"
 poetry run styler2_0 PREPROCESSING --project_dir "$1" --model ANN
 
-echo "Preprocessing $1 for model N_GRAM"
-poetry run styler2_0 PREPROCESSING --project_dir "$1" --model N_GRAM
+echo "Preprocessing $1 for model NGRAM"
+poetry run styler2_0 PREPROCESSING --project_dir "$1" --model NGRAM
